cbdc10abded97b8d6f26b3eedf09fa8001f91f52
hasanmehmoodgithub <136714829+hasanmehmoodgithub@users.noreply.github.com>
2023-06-19 11:59:21 +0500

    cors fixes

--- lib/src/app_urls.dart
+++ lib/src/app_urls.dart
@@ -1,17 +1,22 @@
 class AppUrls{
-  static const String urlAboutImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81qAEsczu67yaN5D0_-tOjlZEKyX9Ca4EsLroLnxg6S54PqgshYC-1DoFEc-dzsYr8rFvtmsc6iL8BwakMoyBpPc7LXR6g=s1600";
-  static const String urlMainImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81p88lnV5IBmWzbJ_NCQQzf4CkzqbaQG0U3IYNbst5TqM-7nbRgfEVi_jwClKi5aOaCMecl9Tt2n18Xr0gapbivaNRxX=s2560";
-  static const String urlBgImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81q98hNuEE34qPygYrs43gBx2TrV9TIFhLCu3n7FIBeqL_DPqUS0WO1bXInn1LNvH45c-epaQAcvJzzKT60dVrk-VCXbUA=s1600";
-  static const String urlEducationImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81oTmQcMLqgMxuZbFtaG03vSFRZqatF0X3sKcJxz2tIrsnu0rp5jTb2liOCoiANEW3Rltatc5wq5Tvf8uS0mE-FwHGmZ=s1600";
-  static const String urlContactImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81ru_MKiXNrknRcz9f9kg4GqOYoa9K06kudT6387jxZNdRVzsKn4-4swFflu3jpOLHbrY9lVb8dPiy3ODe3br-XvYQQtCg=s1600";
-  static const String urlProjectImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81qWSuH66tIj3das3u4ifqyrczd7PV3l4oK9QJaxzoiIL8htWxf45lcgaKjV7litUxTc-318fnbyfVQzyJRblwAYoa6dKg=s1600";
 
-  //static const String urlCv='https://drive.google.com/file/d/1cS02eEmsdngBvgDpAZNyv49hZwDnOSra/view?usp=sharing';
+  static const String urlAboutImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/im1.png?alt=media&token=cde58ef3-5eb7-4932-9da0-6da3f4fe9476";
+
+  static const String urlMainImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/imh.png?alt=media&token=0faab93d-63f1-44f0-b474-d1b7d79689ef";
+
+  static const String urlBgImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/bg.png?alt=media&token=90b7da56-3cb1-4443-9a71-8da8c985f4da";
+
+  static const String urlEducationImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/education.png?alt=media&token=6780c8fc-2f17-4f10-ab42-5697b0b32366";
+
+  static const String urlContactImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/contact.png?alt=media&token=520456a9-153c-4075-89d2-8e1042f04918";
+
+  static const String urlProjectImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/proj1.png?alt=media&token=d4e19d28-211e-4169-8836-5fda155b79b3";
+
   static const String urlCv='https://drive.google.com/file/d/1pn3YznZefN7rTi3nAXsqnyEBWjhH-FH4/view?usp=sharing';
 
   static const String urlLinkedIn='https://www.linkedin.com/in/hasan-mehmood-90331a207/';
 
-  static const String urlGemImage="https://lh3.googleusercontent.com/drive-viewer/AFGJ81pkXhOFhq3cr_9_vEuNEjmsSdsDriYKSRwOAlH71jZx01nFt9xLoqpbHC1V0ue4H_5GfLkkboLgGN8qe0o1rEKWA58BSw=s2560";
+  static const String urlGemImage="https://firebasestorage.googleapis.com/v0/b/hasanmehmoodportfolio.appspot.com/o/1623839863675%201.png?alt=media&token=1414ebf5-c1bc-4fbb-a587-08ba10e52a3a";
 
 
 }
